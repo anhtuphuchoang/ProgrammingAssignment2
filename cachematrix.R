91dc78e4974fa981cad7cf2b71b8fc2e1d6711a3
anhtuphuchoang <tuhoangderrick@gmail.com>
2020-07-22 14:08:42 -0400

    Submission by Anh-Tu, Phuc Hoang

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,29 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  mcmx <-- matrix()
+  if (identical(mcmx,x)){
+    if (is.null(setcache)){
+    Print("Matrix contains no data. Try again.")
+    }else{
+      return (setcache)
+    }
+  }else{
+    mcmx <- x
+  }
+  setcache<<- solve(mcmx)
+  return (setcache)
+  ## initializes an empty matrix. compares it to the argument for x, and if x is not NULL and the cache is empty, solves for the inverse.
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  my<-makeCacheMatrix()
+  if (identical(solve(my),x)){
+    print (my)
+  }else{
+    setnew <- makeCacheMatrix(x)
+    print (setnew)
+  }
+  ## initializes the my variable and compares it to the argument for x. If there is a match it prints from the cache else it updates the cache.
 }
